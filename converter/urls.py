84a2a1fe3872e1eede40b1d49384d246d2432386
fredrexsalac <fredrexsalac@gmail.com>
2025-08-23 16:50:27 +0800

    Update urls.py

--- converter/urls.py
+++ converter/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'converter'
@@ -11,4 +12,11 @@ urlpatterns = [
     path('home/', views.home, name='home'),                 # actual homepage
     path('result/', views.result, name='result'),
     path('process/', views.process, name='process'),
+
+    # Google Search Console verification file
+    path(
+        "googlef7a4e737ec62d8fa.html",
+        TemplateView.as_view(template_name="googlef7a4e737ec62d8fa.html"),
+        name="google-site-verification",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
