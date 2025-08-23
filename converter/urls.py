92f3e08df7dbc9be3966a3fe1af3bcd3b1afcb84
fredrexsalac <fredrexsalac@gmail.com>
2025-08-23 17:07:36 +0800

    Update urls.py

--- converter/urls.py
+++ converter/urls.py
@@ -2,13 +2,20 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
+from django.contrib.sitemaps.views import sitemap
 from . import views
+from .sitemaps import StaticViewSitemap  # you'll create this file next
 
 app_name = 'converter'
 
 # Function to serve Google verification file
 def google_verification(request):
     return HttpResponse("google-site-verification: googlef7a4e737ec62d8fa.html", content_type="text/plain")
+
+# Sitemap dictionary
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('', views.redirect_to_loading, name='redirect'),  # redirect from `/` to `/loading/`
@@ -19,4 +26,7 @@ urlpatterns = [
 
     # Google Search Console verification route
     path("googlef7a4e737ec62d8fa.html", google_verification, name="google-site-verification"),
+
+    # Sitemap
+    path("sitemap.xml", sitemap, {'sitemaps': sitemaps}, name="sitemap"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
