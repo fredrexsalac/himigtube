550f7e4f9e557e50aee9135693857883d47ca7aa
fredrexsalac <fredrexsalac@gmail.com>
2025-08-23 16:58:37 +0800

    Update urls.py

--- converter/urls.py
+++ converter/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
+from django.http import HttpResponse
 from . import views
 
 app_name = 'converter'
+
+# Function to serve Google verification file
+def google_verification(request):
+    return HttpResponse("google-site-verification: googlef7a4e737ec62d8fa.html", content_type="text/plain")
 
 urlpatterns = [
     path('', views.redirect_to_loading, name='redirect'),  # redirect from `/` to `/loading/`
@@ -13,10 +17,6 @@ urlpatterns = [
     path('result/', views.result, name='result'),
     path('process/', views.process, name='process'),
 
-    # Google Search Console verification file
-    path(
-        "googlef7a4e737ec62d8fa.html",
-        TemplateView.as_view(template_name="googlef7a4e737ec62d8fa.html"),
-        name="google-site-verification",
-    ),
+    # Google Search Console verification route
+    path("googlef7a4e737ec62d8fa.html", google_verification, name="google-site-verification"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
