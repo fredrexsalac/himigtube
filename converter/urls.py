65d1c5a9e4bd86d658917a8963013dfbd23a0c0a
fredrexsalac <fredrexsalac@gmail.com>
2025-09-30 16:47:36 +0800

    Update urls.py

--- converter/urls.py
+++ converter/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from django.http import HttpResponse
-from django.contrib.sitemaps.views import sitemap
-from . import views
-from .sitemaps import StaticViewSitemap 
 
 app_name = 'converter'
 
@@ -17,34 +13,4 @@ urlpatterns = [
     path('process/', views.process, name='process'),
     path('video-upload/', views.video_upload, name='video_upload'),  # video upload processing
     path('api/search-suggestions/', views.search_suggestions, name='search_suggestions'),  # API for search suggestions
-    
- # Google Search Console verification
-    path("googlef7a4e737ec62d8fa.html", google_verification, name="google-site-verification"),
-
-    # Sitemap
-    path("sitemap.xml", sitemap, {'sitemaps': sitemaps}, name="sitemap"),
-
-    # Robots.txt
-    path("robots.txt", robots_txt, name="robots"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-app_name = 'converter'
-
-# Google verification
-def google_verification(request):
-    return HttpResponse("google-site-verification: googlef7a4e737ec62d8fa.html", content_type="text/plain")
-
-# robots.txt
-def robots_txt(request):
-    content = """User-agent: *
-Disallow: /process/
-Allow: /
-
-Sitemap: https://himigtube.onrender.com/sitemap.xml
-"""
-    return HttpResponse(content, content_type="text/plain")
-
-# Sitemap dict
-sitemaps = {
-    'static': StaticViewSitemap,
-}
